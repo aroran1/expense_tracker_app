861b9da868df3fd45076d5b726c046147f6ad9f9
aroran1 <nidhigulati2001@gmail.com>
2021-04-25 23:46:22 +0100

    added auto closing model sheeet with Navigator and converted NewTransaction to stateful to fix input auto-clearing bug

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class NewTransaction extends StatefulWidget {
   final Function handleAddTransaction;
 
   NewTransaction(this.handleAddTransaction);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   void submitData() {
     final enteredTitle = titleController.text;
@@ -14,10 +21,15 @@ class NewTransaction extends StatelessWidget {
     if (enteredTitle.isEmpty || enteredAmount <= 0) {
       return;
     }
-    handleAddTransaction(
+
+    // widget.<constructorMethodName> gives access to the method pass in the related Class
+    widget.handleAddTransaction(
       enteredTitle,
       enteredAmount,
     );
+
+    // closes the top most layer
+    Navigator.of(context).pop();
   }
 
   @override
