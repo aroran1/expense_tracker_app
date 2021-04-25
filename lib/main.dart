62660406f8d5a7f9b482d9c42a1c4468e00cb23d
aroran1 <nidhigulati2001@gmail.com>
2021-04-25 23:37:54 +0100

    added modal sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
+import 'package:expense_tracker_app/widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
-import './widgets/user_transaction.dart';
+import 'package:uuid/uuid.dart';
+
+// import './widgets/user_transaction.dart';
+import './models/transaction.dart';
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,8 +40,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Only Stateless widget is needed
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      id: Uuid().v4(),
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _showAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +94,12 @@ class MyHomePage extends StatelessWidget {
         title: Text('Expense Tracker'),
         backgroundColor: Colors.purple,
         centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _showAddNewTransaction(context),
+          )
+        ],
       ),
       // wrap Column inside SingleChildScrollView at the body leve for fullpage scrolling
       // or add scrolling to a particula widget alone as transactionList widget
@@ -53,10 +115,15 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          UserTransactions()
+          // UserTransactions()
           // NewTransaction(),
-          // TransactionalList(),
+          TransactionalList(_userTransactions),
         ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _showAddNewTransaction(context),
       ),
     );
   }
